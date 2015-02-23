66a9c78d643b4104bedb4b291f045bca312dbcba
Duane M. Moody <dmmoody@gmail.com>
2015-02-23 15:29:23 -0800

    add ability to provide reviews

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -4,10 +4,16 @@ restaurant.controller('RestaurantCtrl', function RestaurantCtrl($scope) {
     $scope.restaurants.push({ name: $scope.restaurantName,
                               location: $scope.restaurantLocation,
                               foodType: $scope.restaurantFoodType,
-                              priceRange: $scope.restaurantPriceRange});
+                              priceRange: $scope.restaurantPriceRange,
+                              reviews: []
+                              });
     $scope.restaurantName = null;
     $scope.restaurantLocation = null;
     $scope.restaurantFoodType = null;
     $scope.restaurantPriceRange = null;
+  };
+  $scope.addReview = function(item) {
+    item.reviews.push(item.restaurantReview);
+    item.restaurantReview = null;
   };
 });
