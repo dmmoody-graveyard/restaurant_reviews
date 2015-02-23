306944bbcf21fe7340e0d129d9f0b025f5764554
Duane M. Moody <dmmoody@gmail.com>
2015-02-23 10:11:25 -0800

    add details about each restaurant

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,7 +1,13 @@
 restaurant.controller('RestaurantCtrl', function RestaurantCtrl($scope) {
   $scope.restaurants = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({ name: $scope.restaurantName });
+    $scope.restaurants.push({ name: $scope.restaurantName,
+                              location: $scope.restaurantLocation,
+                              foodType: $scope.restaurantFoodType,
+                              priceRange: $scope.restaurantPriceRange});
     $scope.restaurantName = null;
+    $scope.restaurantLocation = null;
+    $scope.restaurantFoodType = null;
+    $scope.restaurantPriceRange = null;
   };
 });
